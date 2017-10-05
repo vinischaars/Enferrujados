82b6600a9be15c00d21f8ac0d83b142b1e743dfe
vschaar <vschaar@PC00BR28913.ad.esi.adp.com>
2017-10-05 09:39:20 -0300

    Adding tests in Sonar

--- src/test/java/enf/eventos/service/EventoServiceTest.java
+++ src/test/java/enf/eventos/service/EventoServiceTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
